fb390db0605a7aa0fa8a63fbcf54b97132c45c33
javohir7777 <javohirjumayev7777@gmail.com>
2025-01-24 20:26:57 +0500

    dasdasd

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const HomePage = () => {
   let homeStl = { backgroundColor: "red", color: "white", fontSize: "25px" };
-  return <div style={homeStl}>HomePage</div>;
+  return <div style={homeStl}>HomePage 2</div>;
 };
 
 export default HomePage;
